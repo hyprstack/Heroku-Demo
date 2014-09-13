2229040d2545c9c4204e72204e4cf1d8690f5688
MMendes999 <mario.c.mendes1985@gmail.com>
2014-09-13 15:56:46 +0100

    adds index.js adds package json

---
+++ index.js
@@ -0,0 +1,27 @@
+var Hapi = require('hapi');
+var cool = require('cool-ascii-faces');
+
+var server = Hapi.createServer('localhost', 8080);
+
+var routes = [
+  {
+    path: '/',
+    method: 'GET',
+    handler: function(req, reply){
+      reply("Hello World");
+    }
+  },
+  {
+    path:"/smiles",
+    method: "GET",
+    hanlder: function(req, replay){
+      reply(cool);
+    }
+  }
+];
+
+server.route(routes);
+
+server.start(function(){
+  console.log("Server info " + server.info.uri);
+});
